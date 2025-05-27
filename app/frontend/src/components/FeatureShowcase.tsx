51a558392e48c3763cf00891a3b4143d4098cee0
CasonL <casonlamothe@gmail.com>
2025-05-26 22:42:24 -0600

    fix(feature-showcase): Remove extraneous static icons from desktop text layout

--- app/frontend/src/components/FeatureShowcase.tsx
+++ app/frontend/src/components/FeatureShowcase.tsx
@@ -533,9 +533,9 @@ const FeatureShowcase = () => {
 
                 {/* Desktop Layout - Text only (animation in sticky sidebar) */}
                 <div className="hidden lg:block">
-                  <div className="absolute -top-8 md:-top-10 left-1/2 -translate-x-1/2">
+                  {/* <div className="absolute -top-8 md:-top-10 left-1/2 -translate-x-1/2">
                     <item.AnimatedIcon size={80} />
-                  </div>
+                  </div> */}
                   <h3
                     className="text-xl md:text-2xl font-bold mt-16 md:mt-20 mb-3 md:mb-4"
                     dangerouslySetInnerHTML={{ __html: item.heading }}

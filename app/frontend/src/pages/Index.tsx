0f94eaee9cc60d0fa2c555259df4cdb2341e4552
CasonL <casonlamothe@gmail.com>
2025-05-27 17:29:25 -0600

    feat(navbar): Set preRelease=true for Navbar on main page to show 'Join Waitlist'

--- app/frontend/src/pages/Index.tsx
+++ app/frontend/src/pages/Index.tsx
@@ -12,7 +12,7 @@ import Footer from "@/components/Footer";
 const Index = () => {
   return (
     <div className="min-h-screen bg-gray-100">
-      <Navbar />
+      <Navbar preRelease={true} />
       <HeroSection />
       <FeatureShowcase />
       <AboutUsSection />

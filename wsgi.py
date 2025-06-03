e655f15e38d64fe28d15018c08f4c6da5f3cbe76
CasonL <casonlamothe@gmail.com>
2025-06-02 20:17:08 -0600

    Feat: Prepare Flask app for Heroku deployment (Gunicorn, psycopg2, wsgi, migrations)

--- wsgi.py
+++ wsgi.py
@@ -12,8 +12,10 @@ sys.path.insert(0, os.path.dirname(current_dir))
 # Import the create_app function
 from app import create_app
 
-# Create the Flask application
-app = create_app()
+# Use 'prod' or 'production' for the config_name in a production environment
+# You can set FLASK_CONFIG environment variable on Heroku
+config_name = os.getenv('FLASK_CONFIG') or 'production'
+app = create_app(config_name)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
